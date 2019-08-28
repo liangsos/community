2bf81ff76a3cca0c88c03a4b115f3335c2d81cab
chenhualiang <liangsos930@gmail.com>
2019-08-28 11:35:54 +0800

    deleted flyway

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `account_id` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `name` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `token` char(36) COLLATE utf8_bin DEFAULT NULL,
-  `gmt_create` bigint(20) DEFAULT NULL,
-  `gmt_modified` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
