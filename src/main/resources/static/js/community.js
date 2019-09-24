37497391293ff144d2ec2aef1058fb24b8ae49c5
chenhualiang <liangsos930@gmail.com>
2019-09-24 17:51:50 +0800

    实现二级评论功能1

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,39 +1,65 @@
+/**
+ *提交回复
+ */
 function post() {
-   var questionId = $("#question_id").val();
-   var content = $("#comment_content").val();
-   if (!content) {
-       layui.use('layer',function () {
-           var layer = layui.layer;
-           layer.msg('不能回复空内容',{icon:5});
-       });
-       //alert("不能回复空内容");
-       return
-   }
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    if (!content) {
+        layui.use('layer', function () {
+            var layer = layui.layer;
+            layer.msg('不能回复空内容', {icon: 5});
+        });
+        //alert("不能回复空内容");
+        return
+    }
 
     $.ajax({
         type: "POST",
         contentType: 'application/json',
         url: "/comment",
         data: JSON.stringify({
-           "parentId":questionId,
-            "content":content,
-            "type":1
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         }),
         success: function (response) {
-           if (response.code == 200){
-               window.location.reload();
-           }else {
-               if (response.code == 2003){
-                   var isAccepted = confirm(response.message);
-                   if (isAccepted){
-                       window.open("https://github.com/login/oauth/authorize?client_id=49d0a583ad7ef700f872&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
-                       window.localStorage.setItem("closable",true);
-                   }
-               }else {
-                   alert(response.message);
-               }
-           }
+            if (response.code == 200) {
+                window.location.reload();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=49d0a583ad7ef700f872&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
         },
         dataType: "json"
     });
+}
+
+/**
+ * 展开二级评论
+ */
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
+
+    //获取二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
 }
